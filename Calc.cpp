4610f7c50872f2d643388bdd2be23850abd230d7
mkan0141 <kanzume0141@gmail.com>
2018-03-24 23:21:42 +0900

    update: gcdとlcmの追加

--- Calc.cpp
+++ Calc.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <vector>
 #define mod 1000000007
 #define rep(i,x) for(int i=0;i<(x);i++)
 using ll = long long;
 using namespace std;
+
+// gcd
+int gcd(int a,int b){
+  if ( ( 0 == a ) || ( 0 == b ) )
+    return 0;
+  while( a != b ){
+    if ( a > b ) a = a - b;
+    else n = b - a;
+  }
+  return a;
+}
+
+//lcm
+int lcm(int a,int b){
+  if ( ( 0 == a ) || ( 0 == b ) )
+    return 0;
+  
+  return ((a / gcd(a, b)) * b); 
+}
 
 //高速累乗計算  
 long long Pow(long long x,long long n){
@@ -51,13 +71,48 @@ rep(i,n+1){
     }
 }
 
+class Combination{
+public:
+  Combination(int n, int m){
+    fact.resize(n+1);
+    rfact.resize(n+1);
+    MOD=m;
+    fact[0] = rfact[0] = 1;
+    for (int i = 1; i < n + 1; ++i) {
+        fact[i] = (fact[i - 1] * i) % MOD;
+        rfact[i] = (rfact[i - 1] * mod_inv(i)) % MOD;
+    }
+  }
 
-int main(){
-  int a,b,n;
-  cin>>n;
-  //cin>>a>>b;
-  //cout<<"is_prime:("<<n<<") ---> "<<is_prime(n)<<endl;
-  cout<<"sieve:("<<n<<") ---> "<<sieve(n)<<endl;
-  //cout<<"pow:("<<a<<","<<b<<") ---> "<<Pow(a,b)<<endl;
-  return 0;
-}
+  ll mod_pow(ll x, int p){
+    ll res = 1;
+    while(p){
+      if(p&1)res = (ll)1 * res * x % MOD;
+      x = ll(1) * x * x % MOD;
+      p >>= 1;
+    }
+    return res;
+  }
+
+  ll mod_inv(int x){
+    return mod_pow(x, MOD - 2);
+  }
+
+  ll nCr(int n, int r){
+    if (!(0 <= r && r <= n)) return 0;
+    return (((fact[n] * rfact[r]) % MOD) * rfact[n - r]) % MOD;
+  }
+
+  void DEBUG(){
+    rep(i,5){
+      rep(j,5)
+    cout<<i << " " << j << " " << nCr(i,j)<<endl;
+      }
+  }
+  
+private:
+  vector<ll> fact;
+  vector<ll> rfact;
+  int MOD;
+};
+
